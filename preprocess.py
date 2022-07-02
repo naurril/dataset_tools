bad0987c1066f93fa8f7323e7afe50b3dff8111c
naurril <naurril@gmail.com>
2022-07-02 21:09:11 +0800

    update

--- preprocess.py
+++ preprocess.py
@@ -73,6 +73,13 @@ def generate_dataset(extrinsic_calib_path, dataset_path, timeslots, lidar_type="
             os.chdir(os.path.join(dataset_path, "calib", "camera", camera))
             for slot in timeslots:
                 os.system("ln -s -f  ../../../../intermediate/calib_motion_compensated/camera/" + camera + "/*." + slot+".json  ./")
+        
+        for camera in camera_list:
+            prepare_dirs(os.path.join(dataset_path, "calib", "aux_camera", camera))
+            os.chdir(os.path.join(dataset_path, "calib", "aux_camera", camera))
+            for slot in timeslots:
+                os.system("ln -s -f  ../../../../intermediate/calib_motion_compensated/aux_camera/" + camera + "/*." + slot+".json  ./")
+
     else:
         os.system("ln -s -f " + os.path.relpath(extrinsic_calib_path) + " ./calib")
 
